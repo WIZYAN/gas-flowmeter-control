4db3fddefcfd98f79d1eb316f9b0c988bd2ef1dc
WIZYAN <849870710@QQ.COM>
2026-09-17 16:01:18 +0800

    V1.6.3.260917 移除tests文件的管理

--- tests/can/stubs/task.h
+++
@@ -1,7 +0,0 @@
-#ifndef TEST_TASK_H
-#define TEST_TASK_H
-#include "H_CanMock.h"
-#define taskENTER_CRITICAL() H_CanMock_EnterCritical()
-#define taskEXIT_CRITICAL() H_CanMock_ExitCritical()
-#endif
-
